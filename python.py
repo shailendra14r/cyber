063de4fed8baff683bde0b62c591520364a97983
shailendra14r <shailendrarajput0860@gmail.com>
2025-05-09 23:00:54 +0530

    port change

--- python.py
+++ python.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,5 @@ def predict():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
